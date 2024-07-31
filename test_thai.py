e5bb55aac82a4b6491ccc7caa39a8a31c95e68af
Panurat <paunavt5@gmail.com>
2024-07-31 19:30:43 +0700

    test check keyword

--- test_thai.py
+++ test_thai.py
@@ -1,10 +1,25 @@
 from pythainlp import word_tokenize
 
-text = "รัฐบาลแจกเงินหมื่นดิจิทัลวอลเล็ต"
+text = "ก้าวไกลด่าการแจกเงินหมื่นดิจิทัลวอลเล็ตของรัฐบาล"
 tokens = word_tokenize(text)
 
 print(tokens)
 
 for token in tokens:
     if token == "ก้าวไกล":
-        print("ก้าวไกลหรือก้าวกามวะเนี้ย")
+        found_gaw_glai = True
+        # ตรวจสอบเงื่อนไขเกี่ยวกับ "ก้าวไกล"
+        if "รัฐบาล" not in tokens:
+            print("ด่าก้าวไกลไม่เกี่ยวกับรัฐบาล")
+        else:
+            print("ด่าก้าวไกลปกป้องรัฐบาล")
+        exit()
+    
+    if token == "รัฐบาล":
+        found_rut_thabaan = True
+        # ตรวจสอบเงื่อนไขเกี่ยวกับ "รัฐบาล"
+        if "ก้าวไกล" not in tokens:
+            print("ปกป้องรัฐบาลอย่างเดียว")
+        else:
+            print("ชื่นชมรัฐบาลแซะก้าวไกล")
+        exit()
